b1169a2477794f7fd41fa75756fe9614ae5fb515
luchkam <sluchin@mail.ru>
2025-08-05 13:06:09 +0300

    Update telegram.js пытаемся исправить реплаи, чтобы не печаталось chatid и text, откатить

--- utils/telegram.js
+++ utils/telegram.js
@@ -1,17 +1,23 @@
 import axios from 'axios'
 
-export async function sendMessageToTelegram(text) {
+export async function sendMessageToTelegram({ chat_id, text, reply_to_message_id }) {
   const url = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`
 
+  const payload = {
+    chat_id,
+    text,
+    parse_mode: 'HTML'
+  }
+
+  if (reply_to_message_id) {
+    payload.reply_to_message_id = reply_to_message_id
+  }
+
   try {
-    const res = await axios.post(url, {
-      chat_id: process.env.CHAT_ID,
-      text: text
-    })
+    const res = await axios.post(url, payload)
     return res.data
   } catch (error) {
     console.error('❌ Ошибка при отправке в Telegram:', error.response?.data || error.message)
     return null
   }
 }
-

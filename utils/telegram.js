537b126ad89661ea880d2d7ed52197b4c4973d65
luchkam <sluchin@mail.ru>
2025-08-05 12:43:03 +0300

    Update telegram.js лишняя скобка

--- utils/telegram.js
+++ utils/telegram.js
@@ -2,14 +2,16 @@ import axios from 'axios'
 
 export async function sendMessageToTelegram(text) {
   const url = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`
-  const res = await axios.post(url, {
-    chat_id: process.env.CHAT_ID,
-    text: text
-  })
-  return res.data
-}
+
+  try {
+    const res = await axios.post(url, {
+      chat_id: process.env.CHAT_ID,
+      text: text
+    })
+    return res.data
   } catch (error) {
     console.error('❌ Ошибка при отправке в Telegram:', error.response?.data || error.message)
     return null
   }
+}
 
